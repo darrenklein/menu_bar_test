ea10f62e7f2ba9815c83085c2edf401526283d1f
Darren Klein <darren.a.klein@gmail.com>
2019-11-24 17:29:49 -0500

    Initial commit

--- MenuBarTest/AppDelegate.swift
+++ MenuBarTest/AppDelegate.swift
@@ -11,23 +11,35 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var window: NSWindow!
-
+    var statusBarItem: NSStatusItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        let statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+        
+        statusBarItem.button?.title = "+"
+        
+        let statusBarMenu = NSMenu(title: "This is my menu title")
+        
+        statusBarItem.menu = statusBarMenu
+        
+        statusBarMenu.addItem(
+            withTitle: "Order a burrito",
+            action: #selector(AppDelegate.orderABurrito),
+            keyEquivalent: "")
 
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        statusBarMenu.addItem(
+            withTitle: "Cancel burrito order",
+            action: #selector(AppDelegate.cancelBurritoOrder),
+            keyEquivalent: "")
+    }
+
+    @objc func orderABurrito() {
+        print("Ordering a burrito!")
+    }
+
+    @objc func cancelBurritoOrder() {
+        print("Canceling your order :(")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ README.md
@@ -0,0 +1,3 @@
+# MenuBarTest
+
+https://8thlight.com/blog/casey-brant/2019/05/21/macos-menu-bar-extras.html
